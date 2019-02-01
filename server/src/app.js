be2766bad9b2a1f6a830c7d9cd92b8605ecd41e7
nalao <nalaona@gmail.com>
2019-02-01 23:09:40 +0700

    unit 4, create controller Restful api

--- server/src/app.js
+++ server/src/app.js
@@ -19,6 +19,32 @@ app.post('/hello', function(req, res){
     res.send('Ok you post - ' + req.body.name)
 })
 
+/* RESTFUL Api for users management  */
+// create user
+app.post('/user', function(req, res){
+    res.send('ສ້າງຜູ້ໃຊ້')
+})
+
+// edit user, suspend, active
+app.put('/user/:userId', function(req, res){
+    res.send('ແກ້ໄຂຜູ້ໃຊ້')
+})
+
+// delete user
+app.delete('/user/:userId', function(req, res){
+    res.send('ລົບຜູ້ໃຊ້ງານ')
+})
+
+// get user by id
+app.get('/user/:userId', function(req, res){
+    res.send('ເບິ່ງຂໍ້ມູນຜູ້ໃຊ້ງານ')
+})
+
+app.get('/users', function(req, res){
+    res.send('ເບິ່ງຂໍ້ມູນຜູ້ໃຊ້ທັງໝົດ')
+})
+
+
 let port = 8080
 
 app.listen(port, function(){
