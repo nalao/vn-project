17d47ec89073224701927c520feece11131c6b46
nalao <nalaona@gmail.com>
2019-02-01 22:27:24 +0700

    unit 2 create and run server

--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,12 @@
-console.log("Hello nodeJS")
+let express = require('express')
+const app = express()
+
+app.get('/status', function(req, res){
+    res.send('hello nodejs server')
+})
+
+let port = 8080
+
+app.listen(port, function(){
+    console.log('server running on ' + port);
+})
