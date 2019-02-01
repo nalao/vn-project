979a1f392af3d3f15fd135d88bcc8ca807a52709
nalao <nalaona@gmail.com>
2019-02-01 22:45:30 +0700

    unit 3, bodyparser

--- server/src/app.js
+++ server/src/app.js
@@ -1,8 +1,22 @@
 let express = require('express')
+let bodyParser = require('body-parser')
+
 const app = express()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
 app.get('/status', function(req, res){
     res.send('hello nodejs server')
+})
+
+app.get('/hello/:person', function(req, res){
+    console.log('hello - ' + req.params.person)
+    res.send('sey hello with ' + req.params.person)
+})
+
+app.post('/hello', function(req, res){
+    res.send('Ok you post - ' + req.body.name)
 })
 
 let port = 8080
